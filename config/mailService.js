d2514632151695f33923e51db6f61575900c7734
ananya goel <123373636+ananyagoel04@users.noreply.github.com>
2024-12-19 21:19:45 +0530

    mailserver aananya

--- config/mailService.js
+++ config/mailService.js
@@ -3,13 +3,18 @@ const nodemailer = require('nodemailer');
 // Load environment variables from .env
 require('dotenv').config();
 
-// Set up the transport with credentials from environment variables
+// Set up the transport with Zoho's SMTP server details
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtppro.zoho.in',
+  port: process.env.SMTP_PORT || 587, 
+  secure: process.env.SMTP_PORT == 465,
   auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS, 
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = transporter;
