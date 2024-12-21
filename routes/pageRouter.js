8396e38e5fffe9d60ac47acdbcff97ae010500b5
ananya goel <123373636+ananyagoel04@users.noreply.github.com>
2024-12-21 09:51:44 +0530

    infrastructure

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -121,7 +121,7 @@ router.get('/admissions', async (req, res) => {
 });
 router.get('/Infrastructure', async (req, res) => {
   try {
-    res.render('infrastructure');
+    res.render('Infrastructure');
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal Server Error');
