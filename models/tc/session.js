3694c73dcaf43a3450b22db2cd61cef909526831
ananya goel <123373636+ananyagoel04@users.noreply.github.com>
2025-02-23 12:45:42 +0530

    session update

--- models/tc/session.js
+++ models/tc/session.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 const sessionSchema = new mongoose.Schema({
-  year: { type: String, required: true },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
+  year: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Session', sessionSchema);
