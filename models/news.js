473d5c92dfd77b27de05ca57d63b88003ecfaeba
ananya goel <123373636+ananyagoel04@users.noreply.github.com>
2025-10-03 12:38:10 +0530

    news heading limit

--- models/news.js
+++ models/news.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Define the News schema
 const newsSchema = new mongoose.Schema({
-    heading: { type: String, required: true, maxlength: 100 },
+    heading: { type: String, required: true, maxlength: 200 },
     newsContent: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
 });
